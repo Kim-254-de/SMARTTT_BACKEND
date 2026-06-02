8bfaceeb3a53883b754323edb51d20b9721431c6
Wayler0 <solomk213@gmail.com>
2026-06-02 14:48:34 -0400

    Remove SQLite development database

--- config/settings/development.py
+++ config/settings/development.py
@@ -5,11 +5,3 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-
-# Use SQLite in development so no PostgreSQL setup is needed
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
-    }
-}
